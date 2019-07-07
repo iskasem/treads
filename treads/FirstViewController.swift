9c123984470e0dd19d6f77da982301cb62e613dc
Islam Kasem <is_kasem@yahoo.com>
2019-07-07 19:40:45 +0300

    first commit

---
+++ treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  treads
+//
+//  Created by Islam Kasem on 29/06/2019.
+//  Copyright © 2019 Islam Kasem. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  treads
+//
+//  Created by Islam Kasem on 29/06/2019.
+//  Copyright © 2019 Islam Kasem. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
